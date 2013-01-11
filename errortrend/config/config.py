369d7ab61d84033830357847bfe9ad1721f82c05
redswallow <redswallowjysc@gmail.com>
2013-01-11 14:15:12 +0800

    add get_section get_option function

--- errortrend/config/config.py
+++ errortrend/config/config.py
@@ -3,35 +3,31 @@
 from ConfigParser import ConfigParser
 import os
 
-class Config(object): 
+class Config(): 
     """Config class holding all data from configuration files."""
     def __init__(self,filename="config.ini"):
-        self._config = ConfigParser()
-        self.filename=filename
-        self.file={}
-        self.url={}
-        self.load(filename)
+        self._parser = ConfigParser()
+        self.filename=os.path.join(os.path.dirname(os.path.realpath(__file__)),filename)
+        self._parser.read(self.filename)
+        self.file=self.get_section('file')
+        self.url=self.get_section('url')
+    
+    def get_section(self,section):
+        """Load configuration section"""
+        return dict(self._parser.items(section))
 
-    def __parse_settings(self):
-        """Parse SETTINGS section of the config file"""
-        result=self.load_config(file_name)
-        for key,value in self._config.items():
-                print " ",option,"=",self.config.get(section,option)
-
-    def load(self, filename):
-        """Load configuration file"""
-        self._config.read(filename)
-        self.file = dict(self._config.items('file'))
-        self.url = dict(self._config.items('url'))
-
-    def load_config(self,filename):
-        try:  
-            if os.path.exists(filename):  
-                config.read(open(file_name,"r")) 
-                return config  
-        except:  
-            print file_name," is not exit"  
+    def get_option(self,key,section=None):
+        """Load configuration option"""
+        if section:
+            return self._parser.get(section,key)
+        else:
+            for s in self._parser.sections():
+                items=self.get_section(s)
+                if key in items and items[key]:
+                    return items[key]
+            return None
 
 if __name__=='__main__':
     c=Config()
-    print c.file,c.url
+    print c.get_section('file')
+    print c.get_option('ex_url')
