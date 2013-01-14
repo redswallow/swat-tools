ea082b004be2774d043486d41919b953fd140548
redswallow <redswallowjysc@gmail.com>
2013-01-14 15:01:59 +0800

    support loading multi-graphs

--- exgraph/content_script/graph.js
+++ exgraph/content_script/graph.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var graphnum=1;
 	var first=true;
 	CONTAINER_TEMPLATE = "<!-- Graph Container %d-->	\n"
 	+ "	<table style='float: left'>	\n"
@@ -26,25 +27,29 @@ $(document).ready(function(){
 	+ "			<td id='EntirePoolInfo%d' class='chart' style='font-size: 12px; visibility: hidden;'>	\n"
 	+ "				<b>Entire Pool:</b>&nbsp;&nbsp;&nbsp; Count: &nbsp; <b><font color='red' id='currCount'></font></b> &nbsp;&nbsp; 1 Weeks ago:&nbsp;<font color='green' id='pastCount'> </font> \n"
 	+ "			</td>				\n" + "		</tr>					\n" + "	</table>					\n";
-$('#detailsTable a[title="Machine Level Trend"]').parent().append('<a id="addgraph">add</a>');
-$('#detailsTable #addgraph').click(function(){
-	if(first){
-		$('#graphdiv').append(CONTAINER_TEMPLATE.replace(/%d/g,"2"))
-		first=false;
-	}
-	getGraphJSON('http://sre.vip.ebay.com/ex/c/trend/graphJSON'+$(this).siblings().eq(0).attr("href"))
+
+	$('#detailsTable a[title="Machine Level Trend"]').parent().append('<a id="addgraph" style="float:right;color:blue;cursor:pointer;">[add]</a>');
+	$('#detailsTable #addgraph').click(function(){
+	var singleurl=$(this).siblings().eq(0).clone()
+	graphnum=graphnum+1;
+	$('#graphdiv').append(CONTAINER_TEMPLATE.replace(/%d/g,graphnum.toString()))
+	if(first){first=false;}
+	getGraphJSON('http://sre.vip.ebay.com/ex/c/trend/graphJSON'+$(this).siblings().eq(0).attr("href"),graphnum)
+	$('#EntirePoolInfo'+graphnum).html(singleurl)
+	$('#EntirePoolInfo'+graphnum).attr('style','font-size:12px;')
 })
 
 
 
-function getGraphJSON(graphJSONUrl){
+function getGraphJSON(graphJSONUrl,graphnum){
 	$.ajax( {
 		url : graphJSONUrl,
 		context : document.body,
 		success : function(data, textStatus, jqXHR) {
-			var JSON = data;
-			var colorObj = ['#11A84B', '#1C1819' ]
-		SRE.loadDyGraph_drawGraph(2,JSON.d[0],550,300,function(minDate, maxDate, yRanges) {SRE.queryGraphData(minDate, maxDate);},colorObj);
+				var JSON = data;
+				var colorObj = ['#11A84B', '#1C1819' ]
+				SRE.loadDyGraph_drawGraph(graphnum,JSON.d[0],550,300,function(minDate, maxDate, yRanges) {SRE.queryGraphData(minDate, maxDate);},colorObj);
+
 		},
 		error : function(jqXHR, textStatus, errorThrown) {
 				//alert('Error: ' + errorThrown);
