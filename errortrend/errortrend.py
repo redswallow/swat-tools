407b4182932fb183461119628cca950d1f5b6cd8
redswallow <redswallowjysc@gmail.com>
2013-01-16 16:55:51 +0800

    load poolname-error graphjson directly

--- errortrend/errortrend.py
+++ errortrend/errortrend.py
@@ -25,64 +25,40 @@ def read_xls(filename):
             cell_elements.append([task_id]+title.split("--")+[assignee])
     return cell_elements
 
-def get_error_trend_url(pool_name,title):
-    url=c.url['ex_url'].replace("[poolname]",pool_name)
-    log("log.txt",url)
-    return url
+def get_graphjson_url(pool_name,title):
+    return c.url['ex_json_url']%(pool_name,title)
 
 @runTime
-def get_error_trend_json(pool_name,title):
-    #get error trend url
-    url=get_error_trend_url(pool_name,title)
+def get_error_trend_json(url):
     page=urllib2.urlopen(url).read()
-    json_val=json.loads(page)
-    max_count=0
-    new_url=None
-    for line in json_val['aaData']:
-        if title in line[0] and line[3]>max_count:
-            new_url=c.url['init_url']+re.findall(r"'(.*?)'",line[0])[0]
-            max_count=line[3]
-    return new_url
+    json_data=json.loads(page)
+    if json_data['d'][0]['y']['v']:
+        return [int(num) for num in json_data['d'][0]['y']['v']]
+    else:
+        return [0]*6 
 
-def open_web_page(url):
-    command="chrome "+'"'+url+'"'
+def open_web_page(url,broswer):
+    command=' '.join((broswer,url))
     log("log.txt",command)
     os.system(command)
 
-def save_web_page(url):
-    page=urllib2.urlopen(url).read()
-    file=open("image.txt","w")
-    file.write(page)
-
-def get_chart_data(url):
-    page=urllib2.urlopen(url).read()
-    charJsonFinder=re.findall('<textarea id=\'chartJson\'>(.*?)</textarea>',page)
-    if charJsonFinder:
-        charJson=eval(charJsonFinder[0].replace('&#034;','\''))
-        data=charJson['d'][0]['y']['v']
-        return [int(num) for num in data]
-    else:
-        return None
-
-def save_image(url,element):
-    task_id,error_type,title,pool_name,assignee=element
-    image_title=' '.join((task_id,title,pool_name))
-    y=get_chart_data(url) if url else [0]*6 
+@runTime
+def save_image(url,filename):
+    y=get_error_trend_json(url) 
     x=range(0,len(y))
     thresholdFlag=True if max(y)<int(c.image['threshold']) else False
-    Plotter.save_image(Plotter.get_image(x,y,thresholdFlag=thresholdFlag),foldername,image_title)
+    Plotter.save_image(Plotter.get_image(x,y,thresholdFlag=thresholdFlag),foldername,filename)
     if thresholdFlag:
-        log("images/%s/traceToClose.txt"%foldername,'%s %d\n%s\n%s\n' % (image_title,sum(y),url,y))
+        log("images/%s/traceToClose.txt"%foldername,'%s %d\n%s\n%s\n' % (filename,sum(y),url,y))
 
 def running_task():
     #grabs task_id,error_type,title,pool_name from queue
     element=queue.get()
     task_id,error_type,title,pool_name,assignee=element
-    #grabs urls of hosts and then grabs chunk of webpage
-    url=get_error_trend_json(pool_name,title)
-    #get_chrome_image(url)
     if lock.acquire():
-        save_image(url,element)
+        url=get_graphjson_url(pool_name,title)
+        image_title=' '.join((task_id,title,pool_name))
+        save_image(url,image_title)
         lock.release()
     #signals to queue job is done
     queue.task_done()
